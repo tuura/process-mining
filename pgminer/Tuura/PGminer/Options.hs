863a67f4b36a4bd52e6780388ac0ce2e81d4ecea
Andrey Mokhov <andrey.mokhov@gmail.com>
2016-04-08 01:29:04 +0100

    Add help option

--- pgminer/Tuura/PGminer/Options.hs
+++ pgminer/Tuura/PGminer/Options.hs
@@ -1,21 +1,25 @@
 module Tuura.PGminer.Options (Options(..), getOptions) where
 
 import Data.Foldable (foldlM)
+import Control.Monad
 import System.Console.GetOpt
 import System.Environment
+import System.Exit
 
 data Options = Options
-    { optSplit  :: Bool
+    { optHelp   :: Bool
     , optInput  :: IO String
     , optOutput :: String -> IO ()
-    , optReport :: Bool }
+    , optReport :: Bool
+    , optSplit  :: Bool }
 
 defaultOptions :: Options
 defaultOptions  = Options
-    { optSplit  = False
+    { optHelp   = False
     , optInput  = getContents
     , optOutput = putStr
-    , optReport = False }
+    , optReport = False
+    , optSplit  = False }
 
 options :: [OptDescr (Options -> IO Options)]
 options =
@@ -27,23 +31,25 @@ options =
       "Output file"
     , Option ['s'] ["split"]
       (NoArg (\opts -> return $ opts { optSplit = True }))
-      "Split traces with multiple event occurrences" ]
+      "Split traces with multiple event occurrences"
+    , Option ['h'] ["help"]
+      (NoArg (\opts -> return $ opts { optHelp = True }))
+      "Show this help message" ]
 
 getOptions :: IO Options
 getOptions = do
-    argv     <- getArgs
-    progName <- getProgName
-    let header = concat [ "Usage: \n"
-                        , progName
-                        , " <inputFilePath>"
-                        , " [-c | --concurrency-report]"
-                        , " [-o | --output <outputFilePath>]"
-                        , " [-s | --split]" ]
-        helpMessage = usageInfo header options
-    case getOpt Permute options argv of
+    argv   <- getArgs
+    result <- case getOpt Permute options argv of
         (opts, [] , []  ) -> foldlM (flip id) defaultOptions opts
         (opts, [f], []  ) -> foldlM (flip id) defaultOptions
                              { optInput = readFile f } opts
         (_   , _  , []  ) -> ioError . userError $
-                             "Multiple input files\n" ++ helpMessage
+                             "Multiple input files"
         (_   , _  , errs) -> ioError . userError $ concat errs
+    when (optHelp result) $ do
+        progName <- getProgName
+        let header = "Usage: " ++ progName ++ " [input file] [OPTION...]"
+            helpMessage = usageInfo header options
+        putStrLn helpMessage
+        exitSuccess
+    return result
